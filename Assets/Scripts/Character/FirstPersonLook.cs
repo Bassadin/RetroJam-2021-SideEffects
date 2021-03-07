a3998fca11c0c5ce88d739e054f91e4dd08dfed7
exolicious <egzon.demaj@hotmail.com>
2021-03-08 00:50:21 +0100

    auto lock on feature progress
    we now target the closest enemy (from center) in the direction we aim to (while right button is pressed)
    now the camera just needs to rotate to that target

--- Assets/Scripts/Character/FirstPersonLook.cs
+++ Assets/Scripts/Character/FirstPersonLook.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
+using System;
 
 public class FirstPersonLook : MonoBehaviour
 {
@@ -10,8 +11,10 @@ public class FirstPersonLook : MonoBehaviour
     public float sensitivity = 1;
     public float smoothing = 2;
     [SerializeField] private Camera fpsCamera;
-    private List<ILockOnAble> lockonAbleTargetsInFOV = new List<ILockOnAble>();
-
+    private List<ILockOnAble> lockonAbleTargetsInFOVLeft = new List<ILockOnAble>();
+    private List<ILockOnAble> lockonAbleTargetsInFOVRight = new List<ILockOnAble>();
+    private bool lockedOn = false;
+    private ILockOnAble lockTarget;
 
     void Reset()
     {
@@ -25,31 +28,80 @@ public class FirstPersonLook : MonoBehaviour
 
     void Update()
     {
-        // Get smooth mouse look.
-        Vector2 smoothMouseDelta = Vector2.Scale(new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y")), Vector2.one * sensitivity * smoothing);
-        appliedMouseDelta = Vector2.Lerp(appliedMouseDelta, smoothMouseDelta, 1 / smoothing);
-        currentMouseLook += appliedMouseDelta;
-        currentMouseLook.y = Mathf.Clamp(currentMouseLook.y, -90, 90);
+        if(!Input.GetMouseButton(1) && (Mathf.Abs(Input.GetAxisRaw("Mouse X")) > 0 || Mathf.Abs(Input.GetAxisRaw("Mouse Y")) > 0)) {
+            // Get smooth mouse look.
+            Vector2 smoothMouseDelta = Vector2.Scale(new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y")), Vector2.one * sensitivity * smoothing);
+            appliedMouseDelta = Vector2.Lerp(appliedMouseDelta, smoothMouseDelta, 1 / smoothing);
+            currentMouseLook += appliedMouseDelta;
+            currentMouseLook.y = Mathf.Clamp(currentMouseLook.y, -90, 90);
 
-        // Rotate camera and controller.
-        transform.localRotation = Quaternion.AngleAxis(-currentMouseLook.y, Vector3.right);
-        character.localRotation = Quaternion.AngleAxis(currentMouseLook.x, Vector3.up);
-
+            // Rotate camera and controller.
+            transform.localRotation = Quaternion.AngleAxis(-currentMouseLook.y, Vector3.right);
+            character.localRotation = Quaternion.AngleAxis(currentMouseLook.x, Vector3.up);
+        }
+        else {
+            if (Mathf.Abs(Input.GetAxisRaw("Mouse X")) > 0) {
+                CheckTargetsInFOV();
+                try {
+                    if (Input.GetAxisRaw("Mouse X") < 0)
+                        lockTarget = lockonAbleTargetsInFOVLeft[0];
+                    else if (Input.GetAxisRaw("Mouse X") > 0)
+                        lockTarget = lockonAbleTargetsInFOVRight[0];
+                    SetCameraCenterOnLockTargetCenter();
+                } catch(Exception arrayOutOfBounds) {
+                    //tried to get a target on either side, when there was none in the fov in that direction
+                }
+            }
+        }
     }
 
     private void FixedUpdate() {
-        StartCoroutine("CheckTargetsInFOV");
+
     }
 
     private void CheckTargetsInFOV() {
-        List<ILockOnAble> tempTargets = new List<ILockOnAble>();
+        List<ILockOnAble> tempTargetsLeft = new List<ILockOnAble>();
+        List<ILockOnAble> tempTargetsRight = new List<ILockOnAble>();
+        //loop through all LockonableTargets in Scene and set those to the specific List
         foreach (ILockOnAble lockonableTarget in SceneController.getLockonableTargets()) {
+            //we get the world positions and form them to the viewport location ( 0,0 is bottom left of camera viewport and 1,1 top right, so everything with an x less than 0.5 is left of center)
             Vector3 screenPoint = fpsCamera.WorldToViewportPoint(lockonableTarget.GetMiddle());
             bool onScreen = screenPoint.z > 0 && screenPoint.x > 0 && screenPoint.x < 1 && screenPoint.y > 0 && screenPoint.y < 1;
-            if (onScreen)
-                tempTargets.Add(lockonableTarget);
+            if (onScreen) {
+                if(screenPoint.x <= 0.5)
+                    tempTargetsLeft.Add(lockonableTarget);
+                else
+                    tempTargetsRight.Add(lockonableTarget);
+                lockonableTarget.SetScreenPoint(screenPoint);
+            }
         }
-        lockonAbleTargetsInFOV = tempTargets;
-        Debug.Log(lockonAbleTargetsInFOV.Count);
+        tempTargetsLeft.Sort(LeftSort);
+        tempTargetsRight.Sort(RightSort);
+        lockonAbleTargetsInFOVLeft = tempTargetsLeft;
+        lockonAbleTargetsInFOVRight = tempTargetsRight;
+    }
+
+    private int LeftSort(ILockOnAble c1, ILockOnAble c2) {
+        if (c1.GetScreenPoint().x < c2.GetScreenPoint().x) {
+            return 1;
+        }
+        else if (c1.GetScreenPoint().x > c2.GetScreenPoint().x) {
+            return -1;
+        }
+        return 0;
+    }
+    private int RightSort(ILockOnAble c1, ILockOnAble c2) {
+        if (c1.GetScreenPoint().x > c2.GetScreenPoint().x) {
+            return 1;
+        }
+        else if (c1.GetScreenPoint().x < c2.GetScreenPoint().x) {
+            return -1;
+        }
+        return 0;
+    }
+
+    private void SetCameraCenterOnLockTargetCenter() {
+        Debug.Log(lockTarget.GetScreenPoint());
+        Debug.Log(lockTarget.GetMiddle());
     }
 }
