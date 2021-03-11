66eb7ce255b4963f71d028d01f9d76477552e6f8
SG_Oliver_HFU <48796188+OliLeisingeHFU@users.noreply.github.com>
2021-03-11 19:24:35 +0100

    Dash bugfixes
    yayayayayay

--- Assets/Scripts/Character/Components/Dash.cs
+++ Assets/Scripts/Character/Components/Dash.cs
@@ -7,8 +7,15 @@ public class Dash : MonoBehaviour
     [SerializeField]
     Rigidbody rigidbody;
     public ItemSystem playeritems;
-    public float _dashTime;
-    public float _dashSpeed;
+    public float _dashTime = 0.5f;
+    public float _dashSpeed = 20;
+    private float timeStamp;
+    public float cooldown = 5;
+    private byte backDash = 0;
+    private byte forDash = 0;
+    private byte leftDash = 0;
+    private byte rightDash = 0;
+    public float TimeBetweenTaps = 1;
 
     void Awake()
     {
@@ -17,58 +24,70 @@ public class Dash : MonoBehaviour
 
     void LateUpdate()
     {
-        if (Input.GetKeyDown(KeyCode.LeftAlt) && playeritems.hasDash)
+
+        if (Input.GetKeyDown(KeyCode.A) && playeritems.hasDash && timeStamp <= Time.time)
         {
-            if(Input.GetKeyDown(KeyCode.A))
+            leftDash++;
+            Invoke("ResetTapTimes", TimeBetweenTaps);
+            if (leftDash == 2)
             {
-                StartCoroutine(DashCoroutine("left"));
-            }else if(Input.GetKeyDown(KeyCode.S))
+                rigidbody.AddForce(Camera.main.transform.right * -_dashSpeed, ForceMode.VelocityChange);
+                timeStamp = Time.time + cooldown;
+                Invoke("StopMovement", _dashTime);
+                Invoke("ResetTapTimes", TimeBetweenTaps);
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.S) && playeritems.hasDash && timeStamp <= Time.time)
+        {
+            backDash++;
+            Invoke("ResetTapTimes", TimeBetweenTaps);
+            if (backDash == 2)
             {
-                StartCoroutine(DashCoroutine("back"));
-            }else if (Input.GetKeyDown(KeyCode.D))
+                rigidbody.AddForce(Camera.main.transform.forward * -_dashSpeed, ForceMode.VelocityChange);
+                timeStamp = Time.time + cooldown;
+                Invoke("StopMovement", _dashTime);
+                Invoke("ResetTapTimes", TimeBetweenTaps);
+            }
+
+        }
+        else if (Input.GetKeyDown(KeyCode.D) && playeritems.hasDash && timeStamp <= Time.time)
+        {
+            rightDash++;
+            Invoke("ResetTapTimes", TimeBetweenTaps);
+            if (rightDash == 2)
             {
-                StartCoroutine(DashCoroutine("right"));
-            }else
+                //rigidbody.velocity = Camera.main.transform.right * _dashSpeed;
+                rigidbody.AddForce(Camera.main.transform.right * _dashSpeed, ForceMode.VelocityChange);
+                timeStamp = Time.time + cooldown;
+                Invoke("StopMovement", _dashTime);
+                Invoke("ResetTapTimes", TimeBetweenTaps);
+            }
+
+        }
+        else if (Input.GetKeyDown(KeyCode.W) && playeritems.hasDash && timeStamp <= Time.time)
+        {
+            forDash++;
+            Invoke("ResetTapTimes", TimeBetweenTaps);
+            if (forDash == 2)
             {
-                StartCoroutine(DashCoroutine("forward"));
+                rigidbody.AddForce(Camera.main.transform.forward * _dashSpeed, ForceMode.VelocityChange);
+                timeStamp = Time.time + cooldown;
+                Invoke("StopMovement", _dashTime);
+                Invoke("ResetTapTimes", TimeBetweenTaps);
             }
         }
     }
 
-    private IEnumerator DashCoroutine(string direction)
+    private void StopMovement()
     {
-        float startTime = Time.time; // need to remember this to know how long to dash
+        rigidbody.velocity = Vector3.zero;
+    }
 
-        switch (direction)
-        {
-            case "right":
-                while (Time.time < startTime + _dashTime)
-                {
-                    transform.Translate(transform.right * _dashSpeed * Time.deltaTime);
-                    yield return null;
-                }
-                break;
-            case "back":
-                while (Time.time < startTime + _dashTime)
-                {
-                    transform.Translate((-transform.forward) * _dashSpeed * Time.deltaTime);
-                    yield return null;
-                }
-                break;
-            case "left":
-                while (Time.time < startTime + _dashTime)
-                {
-                    transform.Translate((-transform.right) * _dashSpeed * Time.deltaTime);
-                    yield return null;
-                }
-                break;
-            default:
-                while (Time.time < startTime + _dashTime)
-                {
-                    transform.Translate(transform.forward * _dashSpeed * Time.deltaTime);
-                    yield return null;
-                }
-                break;
-        }
+    private void ResetTapTimes()
+    {
+        backDash = 0;
+        forDash = 0;
+        leftDash = 0;
+        rightDash = 0;
     }
 }
