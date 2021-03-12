61be501bcb614766b187bf12f448bd868db34b7b
Bastian Hodapp <bastianhodapp@gmail.com>
2021-03-12 12:44:58 +0100

    Fix not being able to shoot weapon in new level

--- Assets/Scripts/Controllers/Entities/Character/PlayerController.cs
+++ Assets/Scripts/Controllers/Entities/Character/PlayerController.cs
@@ -2,7 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : CharacterController {
+public class PlayerController : CharacterController
+{
 
     [SerializeField] private Camera firstPersonCamera;
     private bool reloading = false;
@@ -15,35 +16,46 @@ public class PlayerController : CharacterController {
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(0)) {
+        if (Input.GetMouseButtonDown(0))
+        {
             ShootWeapon();
         }
-        else if (Input.GetKeyDown(KeyCode.R)) {
+        else if (Input.GetKeyDown(KeyCode.R))
+        {
             StartCoroutine("StartReload");
         }
     }
 
-    public override void ShootWeapon() {
-        if (reloading) {
+    public override void ShootWeapon()
+    {
+        if (reloading)
+        {
             Debug.Log("Currently reloading");
             return;
         }
-        currentWeapon.currentAmmo--;
-        updateAmmoInUIController();
-        GameObject projectileInstance = Instantiate(currentWeapon.projectilePrefab, currentWeapon.projectileSpawnPosition.position, firstPersonCamera.transform.rotation);
-        ProjectileController projectileController = projectileInstance.GetComponent<ProjectileController>();
-        projectileController.transform.forward = firstPersonCamera.transform.forward;
-        projectileController.rigidBody.AddForce(projectileController.transform.forward * currentWeapon.thrust, ForceMode.Impulse);
+
+        if (currentWeapon.currentAmmo > 0)
+        {
+            currentWeapon.currentAmmo--;
+            updateAmmoInUIController();
+            GameObject projectileInstance = Instantiate(currentWeapon.projectilePrefab, currentWeapon.projectileSpawnPosition.position, firstPersonCamera.transform.rotation);
+            ProjectileController projectileController = projectileInstance.GetComponent<ProjectileController>();
+            projectileController.transform.forward = firstPersonCamera.transform.forward;
+            projectileController.rigidBody.AddForce(projectileController.transform.forward * currentWeapon.thrust, ForceMode.Impulse);
+        }
+
     }
 
-    public IEnumerator StartReload() {
+    public IEnumerator StartReload()
+    {
         reloading = true;
         yield return new WaitForSeconds(currentWeapon.reloadTime);
         currentWeapon.currentAmmo = currentWeapon.maxAmmo;
         reloading = false;
         updateAmmoInUIController();
     }
-    private void updateAmmoInUIController() {
+    private void updateAmmoInUIController()
+    {
         UIMasterController.Instance.setAmmunitionAmount(currentWeapon.currentAmmo, currentWeapon.maxAmmo);
     }
 }
